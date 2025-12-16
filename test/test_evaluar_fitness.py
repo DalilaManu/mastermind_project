97043d517898b16fca2a91d3e82126ad59e94e97
DalilaManu <dmoliveira1205@gmail.com>
2025-12-16 11:11:32 +0100

    test(test_evaluar_fitness): :white_check_mark: Añadir test_evaluar_fitness

---
+++ test/test_evaluar_fitness.py
@@ -0,0 +1,16 @@
+from src.mastermind import  GENES, ALELOS, evaluar_fitness
+import pytest
+
+def test_evaluar_fitness():
+    codigo_secreto = ['ROJO', 'AZUL', 'VERDE', 'AMARILLO']
+    individuo_correcto = ['ROJO', 'AZUL', 'VERDE', 'AMARILLO']
+    individuo_parcial = ['ROJO', 'MORADO', 'VERDE', 'NARANJA']
+    individuo_incorrecto = ['NARANJA', 'MORADO', 'AZUL', 'VERDE']
+    
+    fitness_correcto = evaluar_fitness(individuo_correcto, codigo_secreto)
+    fitness_parcial = evaluar_fitness(individuo_parcial, codigo_secreto)
+    fitness_incorrecto = evaluar_fitness(individuo_incorrecto, codigo_secreto)
+    
+    assert fitness_correcto == GENES
+    assert fitness_parcial == 2
+    assert fitness_incorrecto == 0
