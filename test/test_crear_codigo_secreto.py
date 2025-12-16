12863adbce872724266e08780492f851a35bdf3d
DalilaManu <dmoliveira1205@gmail.com>
2025-12-16 22:21:49 +0100

    refactor: Corregir tests

--- test/test_crear_codigo_secreto.py
+++ test/test_crear_codigo_secreto.py
@@ -1,18 +1,15 @@
 
 
 
-from src.mastermind import crear_codigo_secreto, GENES, ALELOS
+from src.mastermind import crear_codigo_secreto, GENES, VALORES_POSIBLES
 import pytest
 
 
 def test_crear_codigo_secreto():
-    codigo = crear_codigo_secreto(GENES, ALELOS)
-    assert len(codigo) == GENES
-    for color in codigo:
-        assert color in ALELOS
-
-
-
+    codigo_secreto = crear_codigo_secreto(GENES, VALORES_POSIBLES)
+    assert len(codigo_secreto) == GENES
+    for color in codigo_secreto:
+        assert color in VALORES_POSIBLES
 
 
     
