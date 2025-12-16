d4b62e39f00166104031a8ac0fe601d4fcbe0f6b
DalilaManu <dmoliveira1205@gmail.com>
2025-12-16 10:23:29 +0100

    refactor: Refactorizar codigo del juego

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,53 +1,37 @@
-import random 
+import random
+
 TAMANHO_GENES = 4
 VALORES_POSIBLES = ['AMARILLO', 'AZUL', 'ROJO', 'VERDE', 'NARANJA', 'MORADO']
 TAMANHO_POBLACION = 100
-TASA_MUTACION = 0.1 
+TASA_MUTACION = 0.1
+
+def crear_codigo_secreto(tamanho_genes, valores_posibles): # Crear un código secreto aleatorio
+    codigo = [] # Inicializar lista vacía para el código
+    for _ in range(tamanho_genes): # Añadir genes al código
+        codigo.append(random.choice(valores_posibles)) # Elegir un color aleatorio
+    return codigo
+
+def crear_individuo(tamanho_genes, valores_posibles):
+    return [random.choice(valores_posibles) for _ in range(tamanho_genes)] # Crear un individuo aleatorio
+
+def crear_poblacion(tamanho_poblacion, tamanho_genes=TAMANHO_GENES, valores_posibles=VALORES_POSIBLES): 
+    return [crear_individuo(tamanho_genes, valores_posibles) for _ in range(tamanho_poblacion)]# Crear una población de individuos
+
+def mutacion(individuo, valores_posibles, tasa_mutacion): # Mutar un individuo con una tasa de mutación dada
+    return [random.choice(valores_posibles) if random.random() < tasa_mutacion else gene  # Mantener el gen original
+            for gene in individuo] # Mutar cada gen según la tasa de mutación
+
+def evaluar_fitness(individuo, codigo_secreto): # Evaluar la aptitud de un individuo comparándolo con el código secreto
+    return sum(ind == cs for ind, cs in zip(individuo, codigo_secreto)) # Contar coincidencias exactas
 
 
-
-def crear_codigo_secreto(tamanho_genes=TAMANHO_GENES, valores_posibles=VALORES_POSIBLES):
-    codigo = []                                      # Creamos una lista vacia donde se van guardando los colores del código secreto
-    for posicion in range(tamanho_genes):            # Repite el proceso las veces que indique tamaño_genes (en este caso, 4) La variable posicion tomarán los valores 0,1,2,3,4
-        valor = random.choice(valores_posibles)      # Selecciona al azar uno de los elementos de valores_posibles, pudiendo repetirse colores **COMO NO REPETIR COLORES**
-        codigo.append(valor)                         # Inserta el color elegido al final de la lista de codigo
-    return codigo                                    # Devuelve 4 colores aleatorios
+def cruce(padre1, padre2): # Realizar cruce entre dos padres para crear dos hijos
+    punto = random.randint(1, TAMANHO_GENES - 1) # Elegir un punto de cruce aleatorio
+    hijo1 = padre1[:punto] + padre2[punto:] # Crear el primer hijo
+    hijo2 = padre2[:punto] + padre1[punto:] # Crear el segundo hijo
+    return hijo1, hijo2   
 
 
-def crear_individuo(TAMANHO_GENES=TAMANHO_GENES, VALORES_POSIBLES=VALORES_POSIBLES):                            
-    individuo = []                                  
-    for posicion in range(TAMANHO_GENES):           
-        valor = random.choice(VALORES_POSIBLES)
-        individuo.append(valor)
-    return individuo
-
-# Uno crea el código secreto a adivinar y el otro crea un individuo de la población que intentará adivinarlo.
-
-def crear_poblacion ():
-    poblacion = []                                  # Creamos una lista vacia donde se van guardando los individuos
-    for individuo in range(TAMANHO_POBLACION):      # Repite el proceso las veces que indique tamaño_poblacion (en este caso, 100)
-        individuo = crear_individuo ()              # Crea un individuo llamando a la función crear_individuo
-        poblacion.append(individuo)                 # Inserta el individuo creado al final de la lista de población
-    return poblacion                                # Devuelve una lista con 100 individuos aleatorios     
-
- 
-
-
-def mutacion(individuo, valores_posibles, tasa_mutacion):
-    individuo_mutado = individuo.copy()     # Creamos una copia del individuo original para no modificarlo directamente
-
-    for i in range(len(individuo_mutado)): # Recorremos cada gen del individuo
-        if random.random() < tasa_mutacion: #Compara ese número con tasa_mutacion. Si es menor, decidimos mutar el gen en la posición i.
-            individuo_mutado[i] = random.choice(valores_posibles) # Si se muta, se asigna un nuevo valor aleatorio del conjunto de valores posibles
-                                                                        #  tasa_mutacion = 0.1, hay 10% de probabilidad de mutar cada gen en cada llamada.
-    return individuo_mutado                                                     # Devuelve el individuo mutado
-
-def evaluar_fitness(individuo, codigo_secreto):
-    aciertos = 0
-    for i in range(len(individuo)):
-        if individuo[i] == codigo_secreto[i]:
-            aciertos += 1
-    return aciertos
 
 
 
