17219c446089f3e2363d2c95fedb4a903db378b3
DalilaManu <dmoliveira1205@gmail.com>
2025-12-18 13:40:17 +0100

    test(test_cruzar_padres): :white_check_mark: Añadir test_cruzar_padres

---
+++ test/test_cruzar_padres.py
@@ -0,0 +1,12 @@
+
+from src.mastermind import cruzar_padres
+
+def test_cruzar_padres():
+    padres = [
+        ['R', 'G', 'B', 'Y'],
+        ['Y', 'B', 'G', 'R']
+    ]
+    hijo = cruzar_padres(padres, genes=4)
+    assert len(hijo) == 4
+    # Verificar que el hijo contiene genes de ambos padres
+    assert all(gene in ['R', 'G', 'B', 'Y'] for gene in hijo)
