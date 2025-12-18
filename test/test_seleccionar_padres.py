5e4d836b5324d316d10120e01a6e3ab1a9388ca2
DalilaManu <dmoliveira1205@gmail.com>
2025-12-18 10:05:59 +0100

    test(test_seleccionar_padres): :white_check_mark: Añadir test_seleccionar_padres

---
+++ test/test_seleccionar_padres.py
@@ -0,0 +1,18 @@
+
+from src.mastermind import seleccionar_padres, crear_individuo
+import pytest
+
+
+def test_seleccionar_padres():
+    codigo_secreto = ['ROJO', 'AZUL', 'VERDE', 'AMARILLO']
+    individuo1 = ['ROJO', 'AZUL', 'VERDE', 'AMARILLO']  # Fitness 4
+    individuo2 = ['ROJO', 'MORADO', 'VERDE', 'NARANJA']  # Fitness 2
+    individuo3 = ['NARANJA', 'MORADO', 'AZUL', 'VERDE']  # Fitness 0
+    individuo4 = ['ROJO', 'AZUL', 'VERDE', 'NARANJA']  # Fitness 3
+
+    poblacion = [individuo1, individuo2, individuo3, individuo4]
+
+    padres = seleccionar_padres(poblacion, codigo_secreto)
+
+    assert len(padres) == 1
+    assert padres[0] == individuo1
