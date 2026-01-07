949be636c768a2b599e0c2ab585d0951b0eca532
DalilaManu <dmoliveira1205@gmail.com>
2026-01-07 14:47:39 +0100

    refactor: Refactorizar main agregando comentarios

--- main.py
+++ main.py
@@ -5,37 +5,47 @@ from src.genetico import (
       seleccionar_padres, 
       cruzar_padres, 
       mutar_individuo,
-        es_solucion)
+        es_solucion
+    )
 from src.constantes_mastermind import GENES, VALORES_POSIBLES, POBLACION, MAX_INTENTOS, TASA_MUTACION
 
 def main():
     # Crear el código secreto y la población inicial
     codigo_secreto = crear_codigo_secreto() 
     print("Código secreto generado. ¡Comienza el juego!")
-    
+
+
+    # Crear población inicial
     poblacion = crear_poblacion(poblacion=POBLACION) 
     intentos = 0
     solucion_encontrada = False
-    
+
+
+    # Bucle principal del algoritmo genético
     while intentos < MAX_INTENTOS and not solucion_encontrada:
         intentos += 1
         print(f"\nIntento {intentos}:")
+
         
-        #Seleccionar padres
+        #Seleccionar los mejores individuos como padres
         padres = seleccionar_padres(poblacion, codigo_secreto) 
-            #Cruzar y mutar para crear un nuevo individuo
-        hijos= []
+          
+        #Crear nueva población a partir de los padres
+        nueva_poblacion = []
+       
         for i in range(POBLACION):
              hijo = cruzar_padres(padres) 
              hijo = mutar_individuo(hijo)
-             hijos.append(hijo)
+             nueva_poblacion.append(hijo)
         
-       #Sustituir la población por el nuevo hijo
-        poblacion = hijos
+   
+        poblacion = nueva_poblacion
         
-        #Evaluar el mejor individuo de la población
+        # Buscar el mejor individuo de la poblacion
         mejor_individuo = poblacion[0]
         mejor_fitness = evaluar_fitness(mejor_individuo, codigo_secreto)
+       
+       
         for individuo in poblacion[1:]:
             fitness_actual = evaluar_fitness(individuo, codigo_secreto)
             if fitness_actual > mejor_fitness:
@@ -47,7 +57,7 @@ def main():
         if es_solucion(mejor_individuo, codigo_secreto):
             solucion_encontrada = True
             print(f"¡Solución encontrada! Código secreto: {codigo_secreto}")
-            break
+           
     
     if not solucion_encontrada:
         print(f"Se han agotado los intentos. El código secreto era: {codigo_secreto}")
