141576b5ee6c62f85a5f1fbfd3e82131924babd5
DalilaManu <dmoliveira1205@gmail.com>
2026-01-13 11:14:36 +0100

    refactor: Refactorizar genetico.py

--- src/genetico.py
+++ src/genetico.py
@@ -4,7 +4,7 @@ from src.constantes_mastermind import POBLACION, GENES, VALORES_POSIBLES, TASA_M
 # Crea un código secreto aleatório para el juego Mastermind
 def crear_codigo_secreto(genes=GENES, valores_posibles=VALORES_POSIBLES): 
     codigo_secreto = []
-    for i in range(genes): # 0 a 3 
+    for i in range(genes):
         codigo_secreto.append(random.choice(valores_posibles))
     return codigo_secreto
 
